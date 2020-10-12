5cf795517e3ed1e52deda9a12bfe8a54e870fe69
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-12 16:29:43 +0530

    validate email of user

--- UserRegProblem.sh
+++ UserRegProblem.sh
@@ -1,26 +1,51 @@
 #!/bin/bash -x
+
+shopt -s extglob
 echo "Welcome To The User Registration"
 
-checkDetails()
-{
+
 read -p "Enter First Name " firstname
 read -p "Enter Last Name " lastname
+read -p "Enter Email-id " email
 
-pattern="^[A-Z][a-z]{2,}$"
+namePattern="^[A-Z][a-z]{2,}$"
 
-if [[ $firstname =~ $pattern ]]
-then
-   echo Valid First Name
-else
-   echo Invalid First Name
-fi
+emailPattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
 
-if [[ $lastname =~ $pattern ]]
-then
-   echo Valid Last Name
-else
-   echo Invalid Last Name
-fi
 
+checkFirstName()
+{
+	firstname=$1
+
+	if [[ $firstname =~ $namePattern ]]
+	then
+		echo Valid First Name
+	else
+		echo Invalid First Name
+	fi
 }
-checkDetails
+checkLastName()
+{
+	lastname=$1
+
+   if [[ $lastname =~ $namePattern ]]
+   then
+      echo Valid Last Name
+   else
+      echo Invalid Last Name
+   fi
+}
+checkEmail()
+{
+	email=$1
+
+	if [[ $email =~ $emailPattern ]]
+	then
+   	echo Valid Email-Id
+	else
+   	echo Invalid Email-Id
+	fi
+}
+checkFirstName $firstname
+checkLastName $lastname
+checkEmail $email
