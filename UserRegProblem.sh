18443a5ffb00bcb483eb2407113b9630fce5664d
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-12 11:35:37 +0530

    Validate First name

--- UserRegProblem.sh
+++ UserRegProblem.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome To The User Registration"
+
+read -p "Enter Your Valid First Name" value
+pattern="^[A-Z]{1}[a-z]{3,}$"
+
+if [[ $value =~ $pattern ]]
+then
+	echo Name is valid
+else
+	echo Invalid Name
+fi
